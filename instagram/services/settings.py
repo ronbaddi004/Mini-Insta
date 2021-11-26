c9579fc025859caa16c8c51bbc1eefe6885f94ef
Rohan Baddi <34314329+ronbaddi004@users.noreply.github.com>
2021-11-26 11:01:43 +0530

    Implemented Caching

--- instagram/services/settings.py
+++ instagram/services/settings.py
@@ -97,6 +97,13 @@ DATABASES = {
 }
 
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211'
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
